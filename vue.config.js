5f33462b791a91f6ed6189ecea023d31440eef14
Nathan Kurt <nate.kurt@gmail.com>
2019-05-28 23:44:31 -0400

    got rid of vue-config... again

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-//const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "cas204/kurt-nathan/week3/" : "/",
-  // configureWebpack: {
-  //   plugins: [new VuetifyLoaderPlugin()]
-  // }
-};
+// //const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+// module.exports = {
+//   publicPath:
+//     process.env.NODE_ENV === "production" ? "cas204/kurt-nathan/week3/" : "/",
+//   // configureWebpack: {
+//   //   plugins: [new VuetifyLoaderPlugin()]
+//   // }
+// };
