32660c1c3fb65fad8ebaec3a519846a7cbeae1b1
Nathan Kurt <nate.kurt@gmail.com>
2019-05-29 00:04:51 -0400

    added vue devtools to prod

--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
 import Vue from "vue";
-import './plugins/vuetify'
-import '@fortawesome/fontawesome-free/css/all.css'
+import "./plugins/vuetify";
+import "@fortawesome/fontawesome-free/css/all.css";
 import App from "./App.vue";
 import router from "./router";
 import Vuetify from "vuetify";
-import VueResource from 'vue-resource'
-Vue.use(VueResource)
+import VueResource from "vue-resource";
+Vue.use(VueResource);
 
 // import vue-github-api
-import GitHubAPI from 'vue-github-api'
-Vue.use(GitHubAPI, { token: "4349722d64498f7a0a34dcabed4214317e516e98" })
+import GitHubAPI from "vue-github-api";
+Vue.use(GitHubAPI, { token: "4349722d64498f7a0a34dcabed4214317e516e98" });
 Vue.config.productionTip = false;
-
 
 //vuetify
 Vue.use(Vuetify, {
   theme: {
-    primary: '#2b7356',
-    secondary: '#732b48',
-    accent: '#2b637c'
+    primary: "#2b7356",
+    secondary: "#732b48",
+    accent: "#2b637c"
   }
-})
+});
 
 // Styles
-require('./styles/scss/main.scss')
-
+require("./styles/scss/main.scss");
+Vue.config.devtools = true;
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ const router = new Router({
       path: "/",
       name: "home",
       component: Home,
-      //meta: { title: "Nate Kurt" }
+      meta: { title: "Nate Kurt" }
     },
     {
       path: "/about",
@@ -22,31 +22,31 @@ const router = new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue"),
-      //meta: { title: "About" }
+      meta: { title: "About" }
     },
     {
       path: "/spartahack",
       name: "spartahack",
-      //meta: { title: "SpartaHack" },
+      meta: { title: "SpartaHack" },
       component: () => import("./views/SpartaHack.vue")
     },
     {
       path: "/protips",
       name: "protips",
       component: () => import("./views/pro-tips.vue"),
-      //meta: { title: "Pro-Tips" }
+      meta: { title: "Pro-Tips" }
     },
     {
       path: "/github",
       name: "github",
       meta: { title: "Nate's GitHub" },
-      //component: () => import("./views/GitHub.vue")
+      component: () => import("./views/GitHub.vue")
     }
   ]
 });
-// router.beforeEach((to, from, next) => {
-//   document.title = to.meta.title;
-//   next();
-// });
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
 
 export default router;
