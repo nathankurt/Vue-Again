f3852466543172c6e430445f69bfffcb209377e2
Nathan Kurt <nate.kurt@gmail.com>
2019-05-30 15:45:56 -0400

    ok changed menu to expansion menu and have it working

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
 
       <v-content>
         <v-container fluid fill-height>
-          <v-layout justify-center align-center>
+          <v-layout wrap justify-center align-center>
             <v-flex shrink>
               <router-view />
             </v-flex>
--- src/views/SpartaHack.vue
+++ src/views/SpartaHack.vue
@@ -8,88 +8,37 @@
     >
     </v-img>
     <h1>Michigan State's Official Student Run Hackathon</h1>
-    <TreeList v-bind:msg='msg'/>
+    <Expansion v-bind:msg='msg1'/>
   </div>
 </template>
 
 <script>
+import Expansion from "@/components/Expansion.vue";
 import TreeList from "@/components/TreeList.vue";
     export default {
-        components: {TreeList},
+        components: {TreeList, Expansion},
         name: "SpartaHack",
         data: () => ({
-      msg: [
-      {
-        id: 1,
-        name: "What is SpartaHack?",
-        children: [
-          { id: 2, name: "SpartaHack is a student run coding marathon that encourages you " },
-          { id: 3, name: "Chrome : app" },
-          { id: 4, name: "Webstorm : app" }
-        ]
-      },
-      {
-        id: 5,
-        name: "Documents :",
-        children: [
+
+      msg1: [
           {
-            id: 6,
-            name: "vuetify :",
-            children: [
-              {
-                id: 7,
-                name: "src :",
-                children: [
-                  { id: 8, name: "index : ts" },
-                  { id: 9, name: "bootstrap : ts" }
-                ]
-              }
-            ]
+              title: "What is SpartaHack?",
+              text: "Spartahack is a student-run coding marathon that encourages you ",
           },
           {
-            id: 10,
-            name: "material2 :",
-            children: [
-              {
-                id: 11,
-                name: "src :",
-                children: [
-                  { id: 12, name: "v-btn : ts" },
-                  { id: 13, name: "v-card : ts" },
-                  { id: 14, name: "v-window : ts" }
-                ]
-              }
-            ]
+              title: "Who can join?",
+              text: "Anyone who is currently a student or has graduated within 12 months of SpartaHack! Graduate students, undergrads, and high-school students from any field of study are welcome."
+          },
+          {
+              title: "Do I need to know how to code?",
+              text: "Definitely not! No programming experience is necessary; we have workshops and mentors at the event to help you learn about a variety of topics."
+          },
+          {
+              title: "When is it?",
+              text: "SpartaHack normally takes place in mid to late January each year! Keep an eye out on it if you go there"
           }
-        ]
-      },
-      {
-        id: 15,
-        name: "Downloads :",
-        children: [
-          { id: 16, name: "October : pdf" },
-          { id: 17, name: "November : pdf" },
-          { id: 18, name: "Tutorial : html" }
-        ]
-      },
-      {
-        id: 19,
-        name: "Videos :",
-        children: [
-          {
-            id: 20,
-            name: "Tutorials :",
-            children: [
-              { id: 21, name: "Basic layouts : mp4" },
-              { id: 22, name: "Advanced techniques : mp4" },
-              { id: 23, name: "All about app : dir" }
-            ]
-          },
-          { id: 24, name: "Intro : mov" },
-          { id: 25, name: "Conference introduction : avi" }
-        ]
-      }
-    ]
+      ]
+      
   })
 
     }
