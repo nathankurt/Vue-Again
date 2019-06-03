1a904b212fcd8a1496fd9c159823cab40618ce5d
Nathan Kurt <nate.kurt@gmail.com>
2019-06-03 13:48:44 -0400

    changed footer so things look better on mobile

--- src/components/VFooter.vue
+++ src/components/VFooter.vue
@@ -1,15 +1,15 @@
 <template>
   <v-footer dark height="auto">
     <v-card class="flex" flat tile>
-      <v-card-title class="teal">
-        <strong class="subheading">Follow Me On Social Media!</strong>
+      <v-card-title class="teal" >
+        <strong class="subheading">Social Media!</strong>
 
         <v-spacer></v-spacer>
 
         <v-btn
           v-for="icon in icons"
           :key="icon"
-          class="mx-3"
+          class="mx-2"
           dark
           icon
           :href="icon.link"
